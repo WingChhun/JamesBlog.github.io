9f42abf90283b80115d2ed8d7bc58f15f7baa265
James Chhun <capt.wing.chhun@gmail.com>
2017-05-30 19:37:42 -0700

    Added some navigation scroll code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
+//Start every Javascript document with doc.ready
 $(document).ready(function () {
-    console.log("Document is ready, loading jQuery...");
-    //Add Scrolling
-})
+    //Sticky Header past Cover Section, 970 refers to height of browser)
+    $(window).scroll(function () {
+        //If 
+        if ($(window).scrollTop() > 970) {
+            //select nav bar, add bootstrap sticky header class
+            $('.navbar').addClass('fixed-top');
+        }
+        if ($(window).scrollTop() < 971) {
+            //remove the navbar-fixed-top class
+            $('.navbar').removeClass('fixed-top');
+        }
+    });
+});
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+        }
+    });
+});
